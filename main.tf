78521e06cae066b8b3b918aab807e529659f395c
Antony Nambikkai <antony.prabhu.nambikkai.-nd@disney.com>
2025-05-01 13:17:46 -0400

    Update AMI Userdata

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 resource "aws_instance" "vm" {  
   ami           = "ami-00304a369b0f2a09c"
   instance_type = "t2.medium"
-
+  user_data = data.template_file.user_data.rendered
   tags = {
     Name = "${var.project}-vm"
   }
 }
+
+
+data "template_file" "user_data" {
+   template = file("${path.module}/userdata.sh")
+ }
+
