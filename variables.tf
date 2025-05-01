df054174ce6910e7d6aa4ac727b9aff0feb8b123
Antony Nambikkai <antony.prabhu.nambikkai.-nd@disney.com>
2025-05-01 16:08:08 -0400

    Update Module

--- variables.tf
+++ variables.tf
@@ -6,3 +6,44 @@ variable "region" {
   type    = string
   default = "us-east-2"
 }
+
+
+variable "vpc_cidr" {
+default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+type = list(string)
+default = ["10.0.1.0/24", "10.0.6.0/24", "10.0.2.0/24"]
+}
+
+variable "alb_name" {
+default = "static-alb"
+}
+
+variable "alb_security_group_ingress" {
+type = list(object({
+from_port = number
+to_port = number
+ protocol = string
+ cidr_blocks = list(string)
+ }))
+ default = [{
+ from_port = 80
+ to_port = 80
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }]
+}
+
+variable "bucket_name" {
+ default = "example-bucket"
+}
+
+variable "bucket_acl" {
+ default = "private"
+}
+
+variable "region" {
+ default = "us-east-1"
+}
