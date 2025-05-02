17d2d687c7b06ef7468bce3cf3b70cbdfd71c10e
Antony Nambikkai <antony.prabhu.nambikkai.-nd@disney.com>
2025-05-01 22:00:01 -0400

    Update Module

--- modules/s3_vpc_endpoint/main.tf
+++ modules/s3_vpc_endpoint/main.tf
@@ -3,5 +3,6 @@
 resource "aws_vpc_endpoint" "s3" {
 vpc_id = var.vpc_id
 service_name = "com.amazonaws.${var.region}.s3"
-route_table_ids = var.route_table_ids
+vpc_endpoint_type = "Interface"
+private_dns_enabled = true
 }
